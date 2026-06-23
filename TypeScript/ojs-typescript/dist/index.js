4c1b5e740c4c174e0cd0795f5f11659f7f3e7c79
AlpOzozer <64591442+AlpOzozer@users.noreply.github.com>
2026-06-23 17:39:53 +0300

    commit

---
+++ TypeScript/ojs-typescript/dist/index.js
@@ -0,0 +1,13 @@
+let firstName = "Uğur";
+let favoriteNumber = 6;
+let isStudent = false;
+firstName = "Alp";
+favoriteNumber = 10;
+isStudent = true;
+const favoriteNumberMessage = "Favori numara: " + favoriteNumber;
+const square = (num) => {
+    return num * num;
+};
+console.log(square(8));
+export {};
+//# sourceMappingURL=index.js.map
---
+++ TypeScript/ojs-typescript/src/index.ts
@@ -0,0 +1,18 @@
+let firstName = "Uğur";
+let favoriteNumber = 6;
+let isStudent = false;
+
+firstName = "Ali";
+
+favoriteNumber = 10;
+
+isStudent = true;
+
+const favoriteNumberMessage = "Favori numara: " + favoriteNumber;
+
+const square = (num: number) => {
+    return num * num;
+};
+
+console.log(square(8));
+console.log(square(9.5));
